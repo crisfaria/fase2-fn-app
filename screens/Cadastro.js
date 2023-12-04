97fb076f6faac132e8d138485321230c0f093e11
Cristina Faria <crisfaria@leandrofaria.com>
2023-12-03 23:01:02 -0300

    Botão de logout tela de Cadastro

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -59,25 +59,43 @@ const CadastroScreen = ({ navigation }) => {
           backgroundColor: "#cceb9d",
           display: "flex",
           flexDirection: "row",
+          justifyContent: "space-between",
           alignItems: "center",
           padding: 30,
           paddingLeft: 15,
           paddingRight: 15,
         }}
       >
-        <View>
-          <Pressable onPress={() => navigation.goBack()}>
-            <Image
-              source={require("../assets/abacaxi.png")}
-              style={{ width: 27, height: 51 }}
-            />
-          </Pressable>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <View>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Image
+                source={require("../assets/abacaxi.png")}
+                style={{ width: 27, height: 51 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={{ fontWeight: "bold", fontSize: 21, marginLeft: 15 }}>
+              CADASTRO
+            </Text>
+            <Text
+              style={{ fontWeight: "400", fontSize: 15, marginLeft: 15 }}
+            ></Text>
+          </View>
         </View>
-        <View>
-          <Text style={{ fontWeight: "bold", fontSize: 21, marginLeft: 15 }}>
-            CADASTRO
-          </Text>
-        </View>
+        <TouchableOpacity onPress={() => alert("fazendo logout")}>
+          <Image
+            source={require("../assets/logout.png")}
+            style={{ width: 51, height: 51, marginRight: 15 }}
+          />
+        </TouchableOpacity>
       </View>
       <ScrollView>
         <View style={{ padding: 15 }}>
